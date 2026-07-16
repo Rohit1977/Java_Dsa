a8e4bf1bbcfb44d8e904b935660733bc2f8f2bee
Rohit1977 <rohitkumawat936@gmail.com>
2026-07-16 22:54:45 +0530

    Time: 0 ms (100%), Space: 43.3 MB (79.75%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,29 +1,22 @@
 class Solution {
-    public int[] plusOne(int[] arr) {
-        StringBuilder sb = new StringBuilder("");
-        int carry = 0;
-        if(arr[arr.length-1] + 1 == 10){
-            carry = 1;
-            sb.append(0);
-        }else{
-            sb.append(arr[arr.length-1]+1);
-        }
-        for(int i=arr.length-2; i>=0; i--){
-            if(arr[i] + carry == 10){
-                sb.insert(0,0);
-                carry = 1;
-            }else{
-                sb.insert(0, arr[i]+carry);
-                carry = 0;
+    public int[] plusOne(int[] digits) {
+
+
+        for (int i = digits.length - 1; i >= 0; i--) {
+
+            
+            if (digits[i] < 9) {
+                digits[i]++;     
+                return digits;    
             }
+            digits[i] = 0;
         }
-        if(carry == 1){
-            sb.insert(0,1);
-        }
-        int[] newArr = new int[sb.length()];
-        for(int i = 0; i<newArr.length; i++){
-            newArr[i] = sb.charAt(i) - '0';
-        }
-        return newArr;
+
+        
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+
+        return result;
     }
 }
+
